27c756b39474fbbe45a694eccb393a0f7fe565c8
himaligandhi <44683089+himaligandhi@users.noreply.github.com>
2020-08-11 13:29:21 -0400

    Update settings.py

--- Trial/settings.py
+++ Trial/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    '/home/site/wwwroot/static',
+    os.path.join(BASE_DIR,'static'),
+]
 
